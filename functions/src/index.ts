a2cc981604743e8a5c05929ca429b5c3228305b6
Bilal Atique <bilalbinatique@gmail.com>
2024-05-09 12:58:03 +0500

    setup functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,19 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as admin from "firebase-admin";
+import { onDocumentCreated } from "firebase-functions/v2/firestore";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+if (!admin.apps.length) admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+exports.createuser = onDocumentCreated(
+  "notifications/{notificationId}",
+  (event) => {
+    const snapshot = event.data;
+    if (!snapshot) {
+      console.log("No data associated with the event");
+      return;
+    }
+    const data = snapshot.data();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+    // send message notification
+  }
+);

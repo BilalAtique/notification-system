d2166e29ebe6b4991e4416b1d9ac4aa0f0560970
Bilal Atique <bilalbinatique@gmail.com>
2024-05-09 11:31:05 +0500

    setup notification button handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+enum NotificationType {
+  "Notification-1",
+  "Notification-2",
+  "Notification-3",
+}
 
 const App = () => {
+  
+  const handleNotificationBtnClick = (notificationType: NotificationType) => {
+    console.log(NotificationType[notificationType]);
+  }
+
   return (
     <div>
-      <button>Button 1</button>
-      <button>Button 2</button>
-      <button>Button 3</button>
+      <button onClick={() => handleNotificationBtnClick(NotificationType["Notification-1"])}>Notification 1</button>
+      <button onClick={() => handleNotificationBtnClick(NotificationType["Notification-2"])}>Notification 2</button>
+      <button onClick={() => handleNotificationBtnClick(NotificationType["Notification-3"])}>Notification 3</button>
     </div>
   );
 };

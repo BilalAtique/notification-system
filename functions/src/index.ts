54f44fe719cb385135fd8a7304eb2bfbd06d6583
Bilal Atique <bilalbinatique@gmail.com>
2024-05-09 21:28:05 +0500

    add send notification in function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,12 @@
 import * as admin from "firebase-admin";
+import { logger } from "firebase-functions/v1";
 import { onDocumentCreated } from "firebase-functions/v2/firestore";
 
 if (!admin.apps.length) admin.initializeApp();
 
-exports.createuser = onDocumentCreated(
+exports.sendNotification = onDocumentCreated(
   "notifications/{notificationId}",
-  (event) => {
+  async (event) => {
     const snapshot = event.data;
     if (!snapshot) {
       console.log("No data associated with the event");
@@ -13,7 +14,19 @@ exports.createuser = onDocumentCreated(
     }
     const data = snapshot.data();
 
-
-    // send message notification
+    const payload = {
+      notification: {
+        title: "New Message",
+        body: snapshot.id
+      },
+    };
+    const token = data.deviceToken;
+    // Send the notification
+    try {
+      const response = await admin.messaging().sendToDevice(token, payload);
+      logger.info("Notification sent successfully:", response);
+    } catch (error) {
+      logger.error("Error sending notification:", error);
+    }
   }
 );

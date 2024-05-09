b8f2bcddd68da50bb21a83fba901ba4adbc5bdcc
Bilal Atique <bilalbinatique@gmail.com>
2024-05-09 13:01:20 +0500

    request notification showing permission on button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
 import { auth, db } from "./firebase/config";
 import SignUp from "./pages/SignUp";
 
@@ -9,22 +9,56 @@ enum NotificationType {
 }
 
 const App = () => {
+  const handleNotificationBtnClick = async (
+    notificationType: NotificationType
+  ) => {
+    try {
+      //add notification in firestore
+      const docRef = await addDoc(collection(db, "notifications"), {
+        userId: auth.currentUser?.uid,
+        type: NotificationType[notificationType],
+        isRead: false,
+      });
+      console.log("Document written with ID: ", docRef.id);
   
-  const handleNotificationBtnClick = async (notificationType: NotificationType) => {
-    console.log(NotificationType[notificationType]);
-    const docRef = await addDoc(collection(db, "notifications"), {
-      userId: auth.currentUser?.uid,
-      type: NotificationType[notificationType],
-      isRead: false
-    });
-    console.log("Document written with ID: ", docRef.id); 
-  }
+      // Request permission for showing notification
+      const permission = await Notification.requestPermission();
+      if (permission === "granted") {
+        console.log("Notification permission granted.");
+      } else {
+        console.log("Notification permission not granted.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
-      <button className="btn btn-outline" onClick={() => handleNotificationBtnClick(NotificationType["Notification-1"])}>Notification 1</button>
-      <button className="btn btn-outline" onClick={() => handleNotificationBtnClick(NotificationType["Notification-2"])}>Notification 2</button>
-      <button className="btn btn-outline" onClick={() => handleNotificationBtnClick(NotificationType["Notification-3"])}>Notification 3</button>
+      <button
+        className="btn btn-outline"
+        onClick={() =>
+          handleNotificationBtnClick(NotificationType["Notification-1"])
+        }
+      >
+        Notification 1
+      </button>
+      <button
+        className="btn btn-outline"
+        onClick={() =>
+          handleNotificationBtnClick(NotificationType["Notification-2"])
+        }
+      >
+        Notification 2
+      </button>
+      <button
+        className="btn btn-outline"
+        onClick={() =>
+          handleNotificationBtnClick(NotificationType["Notification-3"])
+        }
+      >
+        Notification 3
+      </button>
       {!auth.currentUser?.uid && <SignUp />}
     </div>
   );
